10f77554c17d009e0d493942fd525e463c2e0b93
Lautaro Insaurralde <88264895+lautinsa@users.noreply.github.com>
2024-03-01 19:13:57 +0000

    ejercicios

---
+++ ejercicios/guiaA_G.py
@@ -0,0 +1,6 @@
+print ("Las máquinas me sorprenden con mucha frecuencia.")
+print()
+print("MENSAJES")
+print("23")
+print("Una persona puede ser llamada "inteligente" si logra engañar a una persona haciéndole
+creer que es un humano."")
---
+++ entrega/guia1/principal.py
@@ -0,0 +1,4 @@
+#print("Taller de AEIPyhton P1 E2")
+#print("Hola","Soy","Lautaro")
+#print( "Hola", "Como", "Estas", end='*')
+#print("hola", "como", "estas", sep='+') 
