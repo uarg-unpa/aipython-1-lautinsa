51103d4795c89026247cc74a8ab1a0997c506776
Lautaro Insaurralde <88264895+lautinsa@users.noreply.github.com>
2024-03-01 19:57:27 +0000

    ejercicios1.0

--- ejercicios/guiaA_G.py
+++ ejercicios/guiaA_G.py
@@ -1,6 +1,8 @@
 print ("Las máquinas me sorprenden con mucha frecuencia.")
 print()
-print("MENSAJES")
-print("23")
-print("Una persona puede ser llamada "inteligente" si logra engañar a una persona haciéndole
-creer que es un humano."")
+print("Aguante River Plate")
+print(23)
+print("Una computadora puede ser llamada \"inteligiente\" si logra engañar a una persona haciéndole creer que es humano.")
+print("texto \"Destacado\" sigue con el texto")
+print('texto "Destacado" sigue con el texto')
+print (6%5)
--- entrega/guia1/principal.py
+++ entrega/guia1/principal.py
@@ -2,3 +2,14 @@
 #print("Hola","Soy","Lautaro")
 #print( "Hola", "Como", "Estas", end='*')
 #print("hola", "como", "estas", sep='+') 
+
+#print("texto \"Destacado\" sigue con el texto")
+#print('texto "Destacado" sigue con el texto')
+
+#print(234*41)
+#print(24/2)
+#print(23+4-4*2/6**4)
+
+#print (type(23))
+
+valor = 23
