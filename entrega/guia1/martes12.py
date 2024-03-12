e28a80ebd777425b1e0de332c96042535607bd86
Lautaro Insaurralde <88264895+lautinsa@users.noreply.github.com>
2024-03-12 19:19:00 +0000

    clase2

--- entrega/guia1/martes12.py
+++ entrega/guia1/martes12.py
@@ -2,4 +2,12 @@
 #continue pasa a la siguiente iteracion
 suma=0
 for i in range (1,30): 
-print (i)
+    print (i)
+    if i > 10:
+        continue
+    suma=suma+i
+print(suma)
+
+num=int(input())
+while num >9:
+    if num%2==2
