3f6cce78b44d56563e614dab90707b9ebb847b22
Lautaro Insaurralde <88264895+lautinsa@users.noreply.github.com>
2024-03-12 18:20:43 +0000

    clase

--- ejercicios/guia8_14.py
+++ ejercicios/guia8_14.py
@@ -1,15 +1,16 @@
-base= (input(""))
-altura= (input(""))
+# base= (input(""))
+# altura= (input(""))
 
-perimetroRectangulo= 2 * (base + altura)
-areaRectangulo= base * altura
+# perimetroRectangulo= 2 * (base + altura)
+# areaRectangulo= base * altura
 
-circuferencia_radio= (input())
+# circuferencia_radio= (input())
 
-perimetro_circunferencia = 2 * 3.14159 * circuferencia_radio
-area_circunferencia = 3.14159 * circuferencia_radio ** 2
+# perimetro_circunferencia = 2 * 3.14159 * circuferencia_radio
+# area_circunferencia = 3.14159 * circuferencia_radio ** 2
 
-print (perimetroRectangulo)
-print (areaRectangulo)
-print(perimetro_circunferencia)
-print(area_circunferencia)
+# print (perimetroRectangulo)
+# print (areaRectangulo)
+# print(perimetro_circunferencia)
+# print(area_circunferencia)
+
---
+++ entrega/guia1/martes12.py
@@ -0,0 +1,5 @@
+#break finaliza el bucle
+#continue pasa a la siguiente iteracion
+suma=0
+for i in range (1,30): 
+print (i)
