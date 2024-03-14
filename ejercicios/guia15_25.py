ecdb7bab54a22039afd41ea1d6ea727a95bac1d9
Lautaro Insaurralde <88264895+lautinsa@users.noreply.github.com>
2024-03-14 21:36:03 +0000

    completaguia1

--- ejercicios/guia15_25.py
+++ ejercicios/guia15_25.py
@@ -47,6 +47,27 @@
 # print(remplazo)
 
 #22
-text="El razonamiento matemático puede considerarse más bien esquemáticamente como el ejercicio de una combinación de dos instalaciones, que podemos llamar la intuición y el ingenio"
-corte= 
-print(corte)
+# frase = "El razonamiento matemático puede considerarse más bien esquemáticamente como el ejercicio de una combinación de dos instalaciones, que podemos llamar la intuición y el ingenio"
+# espacios_cortados = 0
+# for i in range(len(frase)):
+#     if frase[i] == ' ':
+#         espacios_cortados += 1
+#         if espacios_cortados == 3:
+#             frase_cortada = frase[i+1:]
+#             break
+
+# print(frase_cortada)
+
+#23 remover espacios en blanco // strip
+# frase="     La ciencia es una ecuación diferencial. La religión es una condición de frontera. "
+# fraseSinEspacio= frase.strip()
+
+# print(fraseSinEspacio)
+
+#24 \n salto en linea 
+# textoSep="El razonamiento matemático puede considerarse más bien esquemáticamente como \n el ejercicio de una combinación de dos instalaciones, que podemos llamar la intuición y el ingenio"
+# print(textoSep)
+
+#25 salto \n, tabulacion \t
+# print("Nombre\tEdad\tPais\tCiudad")
+# print("Alexa\t250\tUSA\tCapeCod")
