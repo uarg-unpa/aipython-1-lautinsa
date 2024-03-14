b4b72ee6ead53b2b2b13e97911fe857730cae03a
Lautaro Insaurralde <88264895+lautinsa@users.noreply.github.com>
2024-03-14 16:41:23 +0000

    12

--- ejercicios/guia8_14.py
+++ ejercicios/guia8_14.py
@@ -1,12 +1,40 @@
-#8
+#8 creo que esta bien
 
-base_rectangulo= int(input("Ingrese la base del rectangulo "))
-altura_rectangulo= int(input("Ingrese la altura del rectangulo "))
-circunferencia=float(input("Ingrese el radio de la circunferencia: "))
+# base_rectangulo= int(input("Ingrese la base del rectangulo: "))
+# altura_rectangulo= int(input("Ingrese la altura del rectangulo: "))
+# circunferencia=float(input("Ingrese el radio de la circunferencia: "))
 
-perimetro_rectangulo= base_rectangulo + base_rectangulo + altura_rectangulo + altura_rectangulo # 2 * (base+altura)
-area_rectangulo=base_rectangulo * altura_rectangulo
-area_circunferencia = 3.14159 * area_circunferencia ** 2
+# perimetro_rectangulo= base_rectangulo + base_rectangulo + altura_rectangulo + altura_rectangulo # 2 * (base+altura)
+# area_rectangulo=base_rectangulo * altura_rectangulo
+# area_circunferencia = 3.14159 * 2
 
-print(f"El perimetro del rectangulo es: {perimetro_rectangulo}")
-print(f"La base de un rectangulo es: {area_rectangulo}")
+# print(f"El perimetro del rectangulo es: {perimetro_rectangulo}")
+# print(f"La base de un rectangulo es: {area_rectangulo}")
+# print(f"L circunferencia segun radio es: {area_circunferencia}")
+
+#9 kg t estatura imc=peso/altura**2
+# pesoKg=float(input("Ingrese su peso en kg: "))
+# estaturaCm=float(input("Ingrese su estatura en cm: "))
+
+# imc= pesoKg/estaturaCm**2
+
+# print(f"Tu indice de masa corporal es: {imc}")
+
+#10 temperatura celsius transformacion a grados a fahrenheit // f= (c * 1,8 ) + 32 
+
+# celcius=int(input("Ingresar temperatura en Celsius: ")) 
+
+# fahrenheit= celcius * 1.8 + 32 
+
+# print(f"De Celcius a Fahrenheit es: {fahrenheit}")
+
+#11 horas trabajas, costo por hora, mostrar lo que corresponde 
+
+# horasTrabajadas=int(input("Horas Trabajadas: "))
+# costoHora=8000
+
+# sueldoHora= horasTrabajadas*costoHora
+
+# print(f"Su sueldo es: {sueldoHora}")
+
+#12 cant a invertir, interes anual y numero de años, mostrar capital por inversion
